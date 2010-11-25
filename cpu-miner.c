ea6c1126ec2837461c1869d93d83877d7d40a2b8
Jeff Garzik <jeff@garzik.org>
2010-11-24 19:50:46 -0500

    Add GPLv2 header.

--- cpu-miner.c
+++ cpu-miner.c
@@ -1,7 +1,10 @@
 /*
-   Copyright 2010 Jeff Garzik
-   Distributed under the MIT/X11 software license, see
-   http://www.opensource.org/licenses/mit-license.php
+ * Copyright 2010 Jeff Garzik
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) 
+ * any later version.  See COPYING for more details.
  */
 
 #define _GNU_SOURCE
