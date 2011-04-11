7ff1280bbbd1feb28ce350e2878a4e65fe94627a
Jeff Garzik <jeff@garzik.org>
2011-04-11 17:32:25 -0400

    util: correct off-by-one in month display

--- util.c
+++ util.c
@@ -80,7 +80,7 @@ void applog(int prio, const char *fmt, ...)
 		f = alloca(len);
 		sprintf(f, "[%d-%02d-%02d %02d:%02d:%02d] %s\n",
 			tm.tm_year + 1900,
-			tm.tm_mon,
+			tm.tm_mon + 1,
 			tm.tm_mday,
 			tm.tm_hour,
 			tm.tm_min,
