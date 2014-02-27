65e196a9320a08723f51e891e6eeec85d0716ef5
pooler <pooler@litecoinpool.org>
2014-02-27 22:23:51 +0100

    Version 2.3.3

--- util.c
+++ util.c
@@ -1,6 +1,6 @@
 /*
  * Copyright 2010 Jeff Garzik
- * Copyright 2012-2013 pooler
+ * Copyright 2012-2014 pooler
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
