07fb67e8af8e0d4098d49c7eed0a3b76253149c7
pooler <pooler@litecoinpool.org>
2015-02-28 20:09:16 +0100

    Allow block version 3

--- cpu-miner.c
+++ cpu-miner.c
@@ -345,7 +345,7 @@ err_out:
 }
 
 #define BLOCK_VERSION_MASK 0x000000ff
-#define BLOCK_VERSION_CURRENT 2
+#define BLOCK_VERSION_CURRENT 3
 
 static bool gbt_work_decode(const json_t *val, struct work *work)
 {
